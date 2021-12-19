9eb1619e97f8375893105156fbc1eb22ccdb51f2
alexrezaei <74854699+alexrezaei@users.noreply.github.com>
2021-12-19 18:25:46 +0100

    update to contact.js

--- js/contact.js
+++ js/contact.js
@@ -17,30 +17,45 @@ form.addEventListener("submit", (e) => {
     message.push("Last Name is required");
   }
   if (
-    subject.length === "" ||
+    subject.value === "" ||
     subject.value == null ||
     validateLength(subject.value, 10) === false
   ) {
+    console.log(subject.value);
     message.push("Subject has to contain more than or 10 letters");
   }
   if (
-    address.length === "" ||
-    address.length == null ||
+    address.value === "" ||
+    address.value == null ||
     validateLength(address.value, 24) === false
   ) {
-    console.log(address.value.length);
     message.push("Address length needs to be atleast 25 characters");
+  }
+  if (email.value === "" || email.value == null || validateEmail === false) {
+    message.push("Email is not correctly formated");
   }
   if (message.length > 0) {
     e.preventDefault();
     error.innerHTML += message.join(", ");
   } else {
     e.preventDefault();
+    message.push("Message successfully sent");
+    error.innerHTML += message.join(", ");
   }
 });
 
 const validateLength = (value, len) => {
-  if (value.trim().length <= len) {
+  if (value.trim().length >= len) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const validateEmail = (email) => {
+  let re = /\S+@\S+\.\S+/;
+  let paternMatches = re.test(email);
+  if (paternMatches === true) {
     return true;
   } else {
     return false;
